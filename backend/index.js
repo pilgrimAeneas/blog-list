e672d0cc2f88cbd95bf1e10374948a02bd443c7a
aeneas <pilgrimaeneas@gmail.com>
2026-02-19 17:43:51 +0300

    Move dotenv to config.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-require("dotenv").config()
+const { PORT } = require("./utils/config")
 const app = require("./app")
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
