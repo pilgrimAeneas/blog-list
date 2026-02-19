a11137d07138823f2ffd97dd1bef99b2a58a8493
aeneas <pilgrimaeneas@gmail.com>
2026-02-20 02:15:19 +0300

    Add mostBlogs

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -6,7 +6,26 @@ const favoriteBlog = blogs => {
   return blogs.reduce((acc, blog) => blog.likes > acc.likes ? blog : acc)
 }
 
+const mostBlogs = blogs => {
+  const authorsRepeated = blogs.reduce((acc, blog) => acc.concat(blog.author), [])
+  const authorsUnique = [...(new Set(authorsRepeated))]
+  const authorScores = authorsUnique.map(author => {
+    return { name: author, score: 0 }
+  })
+
+  authorsRepeated.forEach(author => {
+    authorScores.find((entry) => entry.name === author).score++
+  })
+
+  return authorScores.reduce((acc, author) => {
+    return author.score > acc.score ? author : acc
+  })
+}
+
+
+
 module.exports = {
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
