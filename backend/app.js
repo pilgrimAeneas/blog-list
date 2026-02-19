1d5417c04178e80324181272f2ce63064cd7fba0
aeneas <pilgrimaeneas@gmail.com>
2026-02-19 17:50:33 +0300

    Move route handling for blogs to blogs controller

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,14 @@
 const { MONGODB_URL } = require("./utils/config")
 const express = require("express")
 const mongoose = require("mongoose")
-const Blog = require("./models/blog")
 const app = express()
+const blogsRouter = require("./controllers/blogs")
 
 mongoose.connect(MONGODB_URL, { family: 4 })
 
 app.use(express.json())
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+app.use("/api/blogs", blogsRouter)
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 module.exports = app
