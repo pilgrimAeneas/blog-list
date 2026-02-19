e69e7617c214de44dbd201d889bea8c6f9772dd9
aeneas <pilgrimaeneas@gmail.com>
2026-02-19 17:09:49 +0300

    Add document toJSON transform function

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,14 @@ const blogSchema = mongoose.Schema({
   likes: Number,
 })
 
+blogSchema.set("toJSON", {
+  transform: (document, object) => {
+    object.id = object._id.toString()
+    delete object._id
+    delete object.__v
+  }
+})
+
 const Blog = mongoose.model("Blog", blogSchema)
 
 mongoose.connect(process.env.MONGODB_URL, { family: 4 })
