95e463b3d4b95a576754c31f27d2ee1aa4a14683
aeneas <pilgrimaeneas@gmail.com>
2026-02-19 16:38:32 +0300

    Initial backend app setup

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model("Blog", blogSchema)
+
+mongoose.connect(process.env.MONGODB_URL, { family: 4 })
+
+app.use(express.json())
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
+})
