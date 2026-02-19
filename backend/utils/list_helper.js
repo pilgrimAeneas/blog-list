114cd98d9845a771e7d2e5d5286e2164a0f8a764
aeneas <pilgrimaeneas@gmail.com>
2026-02-20 00:26:57 +0300

    Add favoriteBlog and group all tests

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,8 +2,11 @@ const totalLikes = blogs => {
   return blogs.reduce((acc, blog) => blog.likes + acc, 0)
 }
 
-
+const favoriteBlog = blogs => {
+  return blogs.reduce((acc, blog) => blog.likes > acc.likes ? blog : acc)
+}
 
 module.exports = {
   totalLikes,
+  favoriteBlog,
 }
