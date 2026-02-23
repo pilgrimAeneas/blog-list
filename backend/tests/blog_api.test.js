8643a932ea66b6863e7f4b7170e368f083f9e908
aeneas <pilgrimaeneas@gmail.com>
2026-02-23 16:43:19 +0300

    Test and refactor get all

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -16,7 +16,21 @@ beforeEach(async () => {
 })
 
 // Tests here
-test("", () => { })
+test("Return correct number of blogs in json format", async () => {
+  const response = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/)
+
+  assert(response.body.length === initialBlogs.length)
+})
+
+test("Blog's unique identifier is name id not _id", async () => {
+  const response = await api.get("/api/blogs")
+  const one_blog = response.body[0]
+  assert(Object.keys(one_blog).includes("id"))
+  assert(!Object.keys(one_blog).includes("_id"))
+})
 
 after(async () => {
   await mongoose.connection.close()
