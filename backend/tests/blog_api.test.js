5aee41e0cda82f5b0ae8d6f1cad6119e6cc61cdc
aeneas <pilgrimaeneas@gmail.com>
2026-02-23 21:30:30 +0300

    Implement delete request handling and tests

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test")
+const { test, after, beforeEach, describe } = require("node:test")
 const assert = require("node:assert")
 const app = require("../app")
 const supertest = require("supertest")
@@ -15,76 +15,118 @@ beforeEach(async () => {
     .map(blog => blog.save()))
 })
 
-test("Return correct number of blogs in json format", async () => {
-  const response = await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/)
+describe("GET/Read part of the API", () => {
+  describe("GET/Read all notes", () => {
+    test("Return correct number of blogs in json format", async () => {
+      const response = await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/)
 
-  assert(response.body.length === initialBlogs.length)
+      assert(response.body.length === initialBlogs.length)
+    })
+
+    test("Returned blog's unique identifier is id not _id", async () => {
+      const response = await api.get("/api/blogs")
+      const one_blog = response.body[0]
+      assert(Object.keys(one_blog).includes("id"))
+      assert(!Object.keys(one_blog).includes("_id"))
+    })
+  })
 })
 
-test("Blog's unique identifier is name id not _id", async () => {
-  const response = await api.get("/api/blogs")
-  const one_blog = response.body[0]
-  assert(Object.keys(one_blog).includes("id"))
-  assert(!Object.keys(one_blog).includes("_id"))
+describe("POST/Create part of the API", () => {
+  test("POST successfully adds a blog to DB", async () => {
+    const testingBlog = {
+      title: "Testing blog",
+      author: "Testing author",
+      url: "test.com",
+      likes: 5,
+    }
+
+    const blogsBefore = await blogsInDB()
+    await api
+      .post("/api/blogs")
+      .send(testingBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/)
+    const blogsAfter = await blogsInDB()
+
+    assert(blogsAfter.length === blogsBefore.length + 1)
+    assert(blogsAfter.map(blog => blog.title).includes(testingBlog.title))
+    assert(blogsAfter.map(blog => blog.author).includes(testingBlog.author))
+    assert(blogsAfter.map(blog => blog.url).includes(testingBlog.url))
+    assert(blogsAfter.map(blog => blog.likes).includes(testingBlog.likes))
+  })
+
+  test("POST blog with no likes defaults to 0", async () => {
+    const noLikesBlog = {
+      title: "No likes blog",
+      author: "No likes author",
+      url: "Nolikes.com",
+    }
+
+    const response = await api
+      .post("/api/blogs")
+      .send(noLikesBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/)
+
+    const newBlogAsResponse = response.body
+    const newBlogInDB = (await blogsInDB())
+      .find(blog => blog.title === noLikesBlog.title)
+
+    assert(newBlogAsResponse.likes === 0)
+    assert(newBlogInDB.likes === 0)
+  })
+
+  test("POST blog with missing url or title sends a status 400", async () => {
+    const pizzaWithNothing = {
+      author: "No one",
+      likes: 5
+    }
+
+    await api
+      .post("/api/blogs")
+      .send(pizzaWithNothing)
+      .expect(400)
+  })
 })
 
-test("POST successfully adds a blog to DB", async () => {
-  const testingBlog = {
-    title: "Testing blog",
-    author: "Testing author",
-    url: "test.com",
-    likes: 5,
-  }
+describe("DELETE/Delete part of the API", () => {
+  test("DELETE blog works correctly", async () => {
+    const blogsBefore = await blogsInDB()
+    const firstBlog = blogsBefore[0]
+    const existingBlogID = firstBlog.id
 
-  const blogsBefore = await blogsInDB()
-  await api
-    .post("/api/blogs")
-    .send(testingBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/)
-  const blogsAfter = await blogsInDB()
+    await api
+      .delete(`/api/blogs/${existingBlogID}`)
+      .expect(204)
 
-  assert(blogsAfter.length === blogsBefore.length + 1)
-  assert(blogsAfter.map(blog => blog.title).includes(testingBlog.title))
-  assert(blogsAfter.map(blog => blog.author).includes(testingBlog.author))
-  assert(blogsAfter.map(blog => blog.url).includes(testingBlog.url))
-  assert(blogsAfter.map(blog => blog.likes).includes(testingBlog.likes))
+    const blogsAfter = await blogsInDB()
+
+    assert(blogsAfter.length + 1 === blogsBefore.length)
+    assert(!blogsAfter.map(blog => blog.id).includes(existingBlogID))
+  })
 })
 
-test("POST blog with no likes defaults to 0", async () => {
-  const noLikesBlog = {
-    title: "No likes blog",
-    author: "No likes author",
-    url: "Nolikes.com",
-  }
+describe("Unknown Endpoints", () => {
+  const unknownEndpointError = { error: "unknown endpoint" }
+  test("Unknown endpoints with DELETE", async () => {
+    const response = await api
+      .get("/api/notblogs")
+      .expect(404)
 
-  const response = await api
-    .post("/api/blogs")
-    .send(noLikesBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/)
+    assert.deepStrictEqual(unknownEndpointError, response.body)
+  })
 
-  const newBlogAsResponse = response.body
-  const newBlogInDB = (await blogsInDB())
-    .find(blog => blog.title === noLikesBlog.title)
+  test("Unknown endpoints with POST", async () => {
+    const response = await api
+      .post("/api/notblogs")
+      .expect(404)
 
-  assert(newBlogAsResponse.likes === 0)
-  assert(newBlogInDB.likes === 0)
-})
-
-test("Missing url or title sends a status 400", async () => {
-  const pizzaWithNothing = {
-    author: "No one",
-    likes: 5
-  }
-
-  await api
-    .post("/api/blogs")
-    .send(pizzaWithNothing)
-    .expect(400)
+    assert.deepStrictEqual(unknownEndpointError, response.body)
+  })
 })
 
 after(async () => {
