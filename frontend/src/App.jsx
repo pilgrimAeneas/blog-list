a10c0b3d1436d42ba3daf191a8ae420ef43809f2
aeneas <pilgrimaeneas@gmail.com>
2026-03-04 23:42:56 +0300

    Implement basic log in and token in state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react"
 import Blog from "./components/Blog"
 import blogService from "./services/blogs"
+import loginServices from "./services/login"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,58 @@ const App = () => {
     )
   }, [])
 
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginServices.login({ username, password })
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    } catch {
+      window.alert("Wrong credentials")
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>
+              username
+              <input type="text"
+                value={username} onChange={({ target }) => setUsername(target.value)} />
+            </label>
+          </div>
+          <div>
+            <label>
+              password
+              <input type="text"
+                value={password} onChange={({ target }) => setPassword(target.value)} />
+            </label>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </>
+    )
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {
+        user !== null
+          ? <>
+            <h2>blogs</h2>
+            <p>{user.name} logged in.</p>
+            {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+          </>
+          :
+          <>
+            <h2>log in to the application</h2>
+            {loginForm()}
+          </>
+      }
     </div>
   )
 }
