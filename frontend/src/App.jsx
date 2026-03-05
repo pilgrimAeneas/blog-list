05d941d80fa60e72f4677d60bfe65067ad48dce4
aeneas <pilgrimaeneas@gmail.com>
2026-03-06 00:16:55 +0300

    Keep blogs sorted in front end by likes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -98,12 +98,14 @@ const App = () => {
             <CreateForm handleCreateBlog={handleCreateBlog} />
           </Togglable>
 
-          {blogs.map(blog =>
-            <Blog
-              key={blog.id}
-              blog={blog}
-              handleLike={() => handleLike(blog)}
-            />)}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map(blog =>
+              <Blog
+                key={blog.id}
+                blog={blog}
+                handleLike={() => handleLike(blog)}
+              />)}
         </>
         : <LoginForm handleLogin={handleLogin} />
       }
